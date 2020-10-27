797b8e86dcb13e0172e0fcb4b22b9e7836826ae8
Pavan Karthick M <56353002+def-pavankarthick@users.noreply.github.com>
2020-10-27 19:05:40 +0530

    Added End date time and Start date time

--- backend/models/eventModel.js
+++ backend/models/eventModel.js
@@ -3,7 +3,8 @@ const eventSchema = new mongoose.Schema({
     eventname: { type: String, required: true,unique:true},
     description: { type: String, required: true },
     date:{type:String,required:true},
-    time:{type:String,required:true},
+    start:{type:String,required:true},
+    end: { type: String, required: true },
     url:{type:String,required:false},
     imagelink:{type:String,required:false}
 });
